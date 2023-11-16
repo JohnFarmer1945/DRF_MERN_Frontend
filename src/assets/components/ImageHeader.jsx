2346079f7d570c6a439b41e0f696b5880cb65365
JohnFarmer1945 <john.farmer@gmx.net>
2023-11-16 20:53:11 +0100

    header image as link Working

--- src/assets/components/ImageHeader.jsx
+++ src/assets/components/ImageHeader.jsx
@@ -1,13 +1,15 @@
-import "../css/ImageHeader.css";
+import { Link } from "react-router-dom";
 
 function ImageHeader() {
   return (
-    <div
-      id="div_head"
-      className="container-fluid p-0 d-flex justify-content-center"
-    >
-      <img src="/images/3heli_less.png" />
-    </div>
+    <Link className="nav-link p-0 " to="/">
+      <div
+        id="div_head"
+        className="container-fluid p-0 d-flex justify-content-center"
+      >
+        <img src="/images/3heli_less.png" />
+      </div>
+    </Link>
   );
 }
 
