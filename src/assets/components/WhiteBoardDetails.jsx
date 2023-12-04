fb39efeae2ed9f3e2c2249ee0dc13735769fc81d
JohnFarmer1945 <john.farmer@gmx.net>
2023-12-04 17:48:05 +0100

    nice

--- src/assets/components/WhiteBoardDetails.jsx
+++ src/assets/components/WhiteBoardDetails.jsx
@@ -2,9 +2,8 @@ const Whiteboard = ({ whiteboard }) => {
   return (
     <div className="col-4">
       <div className="card border-dark mb-3">
-        <div className="card-header">{whiteboard.createdAt}</div>
+        <div className="card-header">{whiteboard.title}</div>
         <div className="card-body text-dark">
-          <h5 className="card-title">{whiteboard.title}</h5>
           <p className="card-text">{whiteboard.description}</p>
         </div>
       </div>
