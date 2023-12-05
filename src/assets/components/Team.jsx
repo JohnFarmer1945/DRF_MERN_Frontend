b1356e1fae05ac58d0f74bf704c3cca8644e035c
JohnFarmer1945 <john.farmer@gmx.net>
2023-12-05 16:16:23 +0100

    Team container as cards with background and shadow

--- src/assets/components/Team.jsx
+++ src/assets/components/Team.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+import "../css/general.css";
+
 // components
 import WhiteBoardDetails from "./WhiteBoardDetails";
 
@@ -21,31 +23,39 @@ function Team() {
 
   return (
     <>
-      <div className="container-fluid border rounded text-center p-2">
-        <h2 className="fw-bold text-decoration-underline">
-          <b>Team:</b>
-        </h2>
-        <h2>Mitflieger / Hospitationen / Einweisung</h2>
-        <h2>N&S Aktualität</h2>
-        <h2>Persönliche Tagesperformance</h2>
+      <div class="card shadow">
+        <div class="card-header bg-light">
+          <h3 class="text-decoration-underline">Team</h3>
+        </div>
+        <div class="card-body">
+          <h3>Mitflieger / Hospitationen / Einweisung</h3>
+          <h3>N&S Aktualität</h3>
+          <h3>Persönliche Tagesperformance</h3>
+        </div>
       </div>
       <br />
-      <div className="container-fluid border rounded text-center p-2">
-        <h2 className="fw-bold text-decoration-underline">
-          <b>Aktuelles Station Rendsburg:</b>
-        </h2>
-        <div className="container-fluid">
-          <div className="row justify-content-center">
-            {whiteboard &&
-              whiteboard.map((singleWhiteBoardEntry) => (
-                <WhiteBoardDetails
-                  key={singleWhiteBoardEntry._id}
-                  whiteboard={singleWhiteBoardEntry}
-                />
-              ))}
+
+      <div class="card shadow">
+        <div class="card-header bg-light fw-bold ">
+          <h3 class="text-decoration-underline">
+            Aktuelles Station Rendsburg:
+          </h3>
+        </div>
+        <div class="card-body ">
+          <div className="container-fluid">
+            <div className="row justify-content-center">
+              {whiteboard &&
+                whiteboard.map((singleWhiteBoardEntry) => (
+                  <WhiteBoardDetails
+                    key={singleWhiteBoardEntry._id}
+                    whiteboard={singleWhiteBoardEntry}
+                  />
+                ))}
+            </div>
           </div>
         </div>
       </div>
+      <br />
     </>
   );
 }
--- src/assets/components/WhiteBoardDetails.jsx
+++ src/assets/components/WhiteBoardDetails.jsx
@@ -1,7 +1,7 @@
 const Whiteboard = ({ whiteboard }) => {
   return (
     <div className="col-4">
-      <div className="card border-dark mb-3">
+      <div className="card shadow mb-3">
         <div className="card-header">{whiteboard.title}</div>
         <div className="card-body text-dark">
           <p className="card-text">{whiteboard.description}</p>
