8d4033eff5177d0095b4d85f6f25dd5a3e967303
JohnFarmer1945 <john.farmer@gmx.net>
2024-01-20 18:17:16 +0100

    Working

--- src/assets/components/Login.jsx
+++ src/assets/components/Login.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isLoading } = useLogin();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(email, password);
+    await login(email, password);
   };
 
   return (
@@ -60,10 +62,15 @@ const Login = () => {
             />
           </div>
 
-          <button type="submit" className="btn btn-primary">
+          <button
+            disabled={isLoading}
+            type="submit"
+            className="btn btn-primary"
+          >
             Login
           </button>
         </form>
+        {error && <div className="error">{error}</div>}
       </div>
     </>
   );
